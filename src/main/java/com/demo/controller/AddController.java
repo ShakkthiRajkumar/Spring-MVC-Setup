d61c364a61ee5e0fb5bdef5c059b2000e2eb736d
Shakkthi <shakkthi@FULL467-PRO.local>
2022-08-22 16:32:56 +0530

    Working code

--- src/main/java/com/demo/controller/AddController.java
+++ src/main/java/com/demo/controller/AddController.java
@@ -2,13 +2,22 @@ package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class AddController {
     @RequestMapping("/add")
-    public String add(){
-        System.out.println("hello");
-        return "index";
+    public ModelAndView add(HttpServletRequest request, HttpServletResponse response){
+        int a = Integer.parseInt(request.getParameter("p1"));
+        int b = Integer.parseInt(request.getParameter("p2"));
+        int c=a+b;
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("result");
+        mv.addObject("result",c);
+//        System.out.println("hello");
+        return mv;
     }
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-    <h1>Message: ${message}</h1>
+    <h1>Message:</h1>
 </body>
 </html>
